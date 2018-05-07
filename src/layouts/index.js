ea27aa8f01380eae19c0652eaa51023597c1b6db
Quadriphobs1 <abiodunquadriadekunle@gmail.com>
2018-05-07 19:23:54 +0100

    Added version 2 dependencies

---
+++ src/layouts/index.js
@@ -0,0 +1,80 @@
+'use strict'
+
+Object.defineProperty(exports, '__esModule', { value: true })
+
+/**
+ * Retrieve context components to global register on Vue
+ * @export
+ * @return {Array} With layout components
+ */
+function layouts () {
+  var layouts = require.context('@/layouts', false, /^\.\/.*\.vue$/)
+  return layouts.keys().map(layouts)
+}
+
+var _Vue = {}
+
+/**
+ * Install Plugin vue-extend-layout
+ *
+ * @param {Vue} Vue
+ * @param {Object} [options={}]
+ */
+function VueExtendLayout (Vue) {
+  // eslint-disable-next-line
+  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {}
+
+  _Vue = Vue
+
+  // Register layouts
+  layouts().forEach(function (c) {
+    c = c.default || c
+    Vue.component(c.name, c)
+  })
+}
+
+/**
+ * Compile the layout
+ *
+ * @param {VueComponent} context Vue instance
+ * @returns Compiled Component
+ */
+function layoutCompile (context) {
+  return _Vue.compile('<' + (context.$route.meta.layout || 'default') + ' />')
+}
+
+/**
+ * Render the layout
+ *
+ * @param {VueComponent} context Vue instance
+ * @param {Object} res Compiled Component
+ * @param {Boolean} update To force update component layout
+ */
+function layoutRender (context, res, update) {
+  context.$options.render = res.render
+  context.$options.staticRenderFns = res.staticRenderFns
+  if (update) context.$forceUpdate()
+}
+
+/**
+ * Mixed to Vue root instance
+ */
+var layout = {
+  beforeCreate: function beforeCreate () {
+    layoutRender(this, layoutCompile(this))
+  },
+
+  watch: {
+    '$route': function $route () {
+      layoutRender(this, layoutCompile(this), true)
+    }
+  }
+}
+
+module.exports = {
+  VueExtendLayout: VueExtendLayout,
+  layout: layout
+}
+
+exports.VueExtendLayout = VueExtendLayout
+exports.layout = layout
---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import '@/assets/scss/neat.scss'
+import 'bootstrap/dist/css/bootstrap-grid.css'
+
+require('bootstrap')
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
